03be88f282941d151968351becd6a817680b51df
carlos300497 <63626758+carlos300497@users.noreply.github.com>
2025-03-18 23:42:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 // Configuración de MQTT
-const broker = 'io.adafruit.com';
-const port = 443; // Puerto para conexión segura MQTT
-const username = 'carlos97M'; // Reemplaza con tu nombre de usuario de Adafruit IO
-const aioKey = 'aio_bjDy87R1fy2USxOIgCxqh91gUqtp'; // Reemplaza con tu clave de Adafruit IO
-const topic = `${username}/feeds/Humedad`; // Cambia esto al feed que estés usando
+const broker = 'broker.emqx.io';
+const port = 8883; // Puerto para conexión segura MQTT
+const topic = 'sensor/soilMoisture'; // Cambia esto al tópico que estés usando
 
 // Crear un cliente MQTT
 const client = new Paho.MQTT.Client(broker, port, 'webClient');
 
 // Función de conexión
 client.connect({
-    userName: username,
-    password: aioKey,
     onSuccess: onConnect,
     useSSL: true,
 });
